045bc0fb18bf0e6a86f0f105b21d798a0a584ff9
Valencia <vlrichard00@gmail.com>
2016-09-26 07:31:42 -0400

    TryOne

---
+++ app/src/main/java/com/example/astoldbyvee/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.astoldbyvee.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+ 
