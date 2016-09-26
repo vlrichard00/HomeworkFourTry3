4075a5f52a3be03165bba7cccbe17c0e30e70dae
Valencia <vlrichard00@gmail.com>
2016-09-26 07:55:17 -0400

    TryOne

--- app/src/main/java/com/example/astoldbyvee/myapplication/MainActivity.java
+++ app/src/main/java/com/example/astoldbyvee/myapplication/MainActivity.java
@@ -22,6 +22,111 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public static Integer getComputerChoice() {
+//Random number for the buttons
+        Random rand = new Random();
+
+        int n = rand.nextInt(8) + 1;
+        if (n == 1) {
+            return R.id.button;
+        } else if (n == 2) {
+            return R.id.button2;
+        } else if (n == 3) {
+            return R.id.button3;
+        } else if (n == 4) {
+            return R.id.button4;
+        } else if (n == 5) {
+            return R.id.button5;
+        } else if (n == 6) {
+            return R.id.button6;
+        } else if (n == 7) {
+            return R.id.button7;
+        } else if (n == 8) {
+            return R.id.button8;
+        } else {
+            return R.id.button;
+        }
+    }
+
+    // for each time a user clicks on button, show an x, and also, and random button will how an o
+    public static Integer cardFlip(Integer buttonChoice) {
+        if (buttonChoice == R.id.button3) {
+            return R.drawable.xx;
+            if (buttonChoice == R.id.button2) {
+                return R.drawable.xx;
+            } else if (buttonChoice == R.id.button) {
+                return R.drawable.xx;
+                {
+                    getComputerChoice()
+                    {
+                        return R.drawable.oh;
+                    }
+                }
+            } else if (buttonChoice == R.id.button4) {
+                return R.drawable.xx;
+                {
+                    getComputerChoice()
+                    {
+                        return R.drawable.oh;
+                    }
+                }
+            } else if (buttonChoice == R.id.button5) {
+                return R.drawable.xx;
+                {
+                    getComputerChoice()
+                    {
+                        return R.drawable.oh;
+                    }
+                }
+            } else if (buttonChoice == R.id.button6) {
+                return R.drawable.xx;
+                {
+                    getComputerChoice()
+                    {
+                        return R.drawable.oh;
+
+                    }
+                }
+            } else if (buttonChoice == R.id.button7) {
+                return R.drawable.xx;
+                {
+                    getComputerChoice()
+                    {
+                        return R.drawable.oh;
+                    }
+                }
+            } else if (buttonChoice == R.id.button8) {
+                return R.drawable.xx;
+                {
+                    getComputerChoice()
+                    {
+                        return R.drawable.oh;
+                    }
+                }
+            } else {
+                return R.drawable.xx;
+                {
+                    getComputerChoice()
+                    {
+                        return R.drawable.oh;
+                    }
+                }
+            }
+        }
+    }
 }
 
- 
+
+    public void  eval( new Random)
+    {
+        // Horizontal Winners
+
+    if()
+
+
+    }
+
+
+
